6d39a0cbd97a94649f025194de8855afe757da37
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-06 14:36:16 -0500

    [feature] understanding preprocessing data, this is the first step for any machine learning model

---
+++ 00-Data_Processing/data_preprocessing_template.py
@@ -0,0 +1,29 @@
+# Data Preprocessing
+
+# Importing The Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the Dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, : -1].values
+y = dataset.iloc[:, 3].values
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+x = onehotencoder.fit_transform(x).toarray()
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+y
+
