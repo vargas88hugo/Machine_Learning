dbbdd5db9507cedffbb72b011d649060c28d9506
Hugo Vargas <vargas88hugo@gmail.com>
2019-11-04 11:56:33 -0500

    [update] added training set and feature scaling

--- 00-Data_Processing/data_preprocessing_template.py
+++ 00-Data_Processing/data_preprocessing_template.py
@@ -25,3 +25,13 @@ x = onehotencoder.fit_transform(x).toarray()
 
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+# Spliting the dataset into the training set and test set
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
